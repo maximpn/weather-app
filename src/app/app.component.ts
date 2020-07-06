32c66dea90722beb8680cc5bcedaa05ad03c523b
Maxim Palenov <maxim.palenov@bloomreach.com>
2020-07-06 21:21:20 +0200

    Prepare the application for development

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'wa-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'weather-app';
