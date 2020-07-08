8187b5fe5a4b6ecf3231a3b6ba41bf91035ab713
Maxim Palenov <maxim.palenov@bloomreach.com>
2020-07-08 16:20:36 +0200

    Move app component into a separate folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app/app.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
 import { httpInterceptorProviders } from './http-interceptors';
 import { City } from './models/city.enum';
