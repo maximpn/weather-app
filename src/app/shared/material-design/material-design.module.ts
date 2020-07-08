3913bcbb1198a7fc5cd4bf1b92950fbd6c3c239d
Maxim Palenov <maxim.palenov@bloomreach.com>
2020-07-08 16:18:24 +0200

    Display hourly forecast

--- src/app/shared/material-design/material-design.module.ts
+++ src/app/shared/material-design/material-design.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   exports: [
     MatToolbarModule,
     MatCardModule,
+    MatIconModule,
+    MatDividerModule,
   ],
 })
 export class MaterialDesignModule {}
