c1adc0f809dc49a3c075877baba17876f968fd8a
Maxim Palenov <maxim.palenov@bloomreach.com>
2020-07-09 12:37:23 +0200

    Test the greeting message properly

--- e2e/integration/app.spec.ts
+++ e2e/integration/app.spec.ts
@@ -1,9 +1,10 @@
-import { getGreeting } from '../support/app.po';
+import { getGreeting, getGreetingDescription } from '../support/app.po';
 
 describe('WeatherApp', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
     getGreeting().contains('Weather App');
+    getGreetingDescription().contains('Shows weather in five European cities');
   });
 });
