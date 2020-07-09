2fdc7fee34d75bfe86c686aa16448315c82f782e
Maxim Palenov <maxim.palenov@bloomreach.com>
2020-07-09 20:10:05 +0200

    Add GitHub repo link

--- src/app/shared/material-design/material-design.module.ts
+++ src/app/shared/material-design/material-design.module.ts
@@ -1,10 +1,14 @@
+// tslint:disable-next-line:match-default-export-name
+import githubIcon from '!!raw-loader!./icons/github-circle-white-transparent.svg';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
-import { MatIconModule } from '@angular/material/icon';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @NgModule({
   exports: [
@@ -14,6 +18,18 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatDividerModule,
     MatSnackBarModule,
     MatProgressBarModule,
+    MatButtonModule,
   ],
 })
-export class MaterialDesignModule {}
+export class MaterialDesignModule {
+  constructor(
+    private readonly iconRegistry: MatIconRegistry,
+    private readonly donSanitizer: DomSanitizer,
+  ) {
+    this.registerCustomIcons();
+  }
+
+  private registerCustomIcons(): void {
+    this.iconRegistry.addSvgIconLiteral('github', this.donSanitizer.bypassSecurityTrustHtml(githubIcon));
+  }
+}
